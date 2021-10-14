6057e7aa1f7e419ece61681e57d22262d6d74b16
大城由晃 <yoshiaki19980217@icloud.com>
2021-10-14 22:44:13 +0900

    Headerのコンポーネント作成

---
+++ src/components/KeyVisual.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const KeyVisual = () => {
+  return (
+    <div className="key-visual">
+      <h2>
+        あなたの優しい心が<br/>
+        子供達への愛になります。
+      </h2>
+    </div>
+  )
+}
+
+export default KeyVisual

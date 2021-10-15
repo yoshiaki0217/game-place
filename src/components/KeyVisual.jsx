a3578e864dd01e65389304e94315749ff538e1f9
大城由晃 <yoshiaki19980217@icloud.com>
2021-10-16 00:52:42 +0900

    /KeyVisualを作成

--- src/components/KeyVisual.jsx
+++ src/components/KeyVisual.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const KeyVisual = () => {
   return (
-    <div className="key-visual">
-      <h2>
-        あなたの優しい心が<br/>
-        子供達への愛になります。
+    <div className="key-visual h-full">
+      <h2 className="pt-32 pl-40 key-visual-title text-5xl text-white leading-loose">
+        あなたの<span className="">優しい心</span>が<br/>
+        子供達への<span className="">愛</span>になります。
       </h2>
     </div>
   )
